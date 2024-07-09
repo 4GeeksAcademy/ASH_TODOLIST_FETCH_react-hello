95aa96ea1d1825fd3bb49f2cb8b22b768af11854
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-09 14:55:12 +0000

    subida final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,7 @@ import React, {useState} from "react";
 const Home = () => {
 	let [task, setTask] = useState("");
 	let [list, setList] = useState([]);
+    let [hoverIndex, setHoverIndex] = useState(null);
 
 	const handleKeyDown = (e) => {
         if (e.key === "Enter") {
@@ -12,10 +13,6 @@ const Home = () => {
             e.preventDefault();
         }
     };
-
-	// function showButton() {
-
-    // }
 
     function deleteTarea(index) {
         const newList = list.filter((_, i) => i !== index);
@@ -32,7 +29,13 @@ const Home = () => {
                 ) : (
                     <ul className="list-group">
                         {list.map((valor, index) => (
-                            <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{valor}<button type="button" className="btn-close float-end" aria-label="Close" onClick={() => deleteTarea(index)}></button></li>
+                            <li 
+                            key={index} 
+                            className="list-group-item d-flex justify-content-between align-items-center" 
+                            onMouseEnter={() => setHoverIndex(index)} 
+                            onMouseLeave={() => setHoverIndex(null)}>
+                            {valor}{hoverIndex === index && (<button type="button" className="btn-close float-end" aria-label="Close" onClick={() => deleteTarea(index)}></button>)}
+                            </li>
                         ))}
                         <li className="list-group-item text-muted small text-start">{list.length} {list.length === 1 ? 'item left' : 'items left'}</li>
                     </ul>
