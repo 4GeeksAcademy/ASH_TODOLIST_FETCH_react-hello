e4ac5753b77aeac6fa5bea9aa260d2e62c7300f0
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-09 21:06:48 +0000

    subida con delete incluido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,71 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
-//create your first component
 const Home = () => {
-	let [task, setTask] = useState("");
-	let [list, setList] = useState([]);
-    let [hoverIndex, setHoverIndex] = useState(null);
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
+    const [hoverIndex, setHoverIndex] = useState(null);
+    const urlAPI4geeks = 'https://playground.4geeks.com/todo'
 
 	const handleKeyDown = (e) => {
         if (e.key === "Enter") {
-			task === "" ? alert("la tarea esta vacia, tontin") : setList(list.concat(task));
+            task === "" ? alert("la tarea esta vacia, tontin") : crearTareas();
             setTask("");
             e.preventDefault();
+            obtenerTareas();
         }
     };
 
-    function deleteTarea(index) {
-        const newList = list.filter((_, i) => i !== index);
-        setList(newList);
+    // function deleteTarea(item) {
+    //     console.log(item.id);
+    // }
+
+    function obtenerTareas() {
+        fetch(urlAPI4geeks+'/users/agusotoholt', {
+            method:'GET',
+            headers:{"Content-Type": "application/json"}
+        })
+        .then((response)=> response.json())
+        .then((data)=> setList(data.todos))
+        .catch((error)=> console.log(error))
     }
+
+    function crearTareas() {
+        fetch(urlAPI4geeks+'/todos/agusotoholt', {
+            method:'POST',
+            body: JSON.stringify({"label": task, "is_done": false}),
+            headers:{"Content-Type": "application/json"}
+        })
+        .then((response)=> response.json())
+        .then((data)=> console.log(data))
+        .catch((error)=> console.log(error))
+    }
+
+    function borrarTareas(item) {
+        fetch(urlAPI4geeks+'/todos/'+item.id, {
+            method:'DELETE',
+            headers:{"Content-Type": "application/json"}
+        })
+        .then((response)=> response.json())
+        .then((data)=> console.log(data))
+        .catch((error)=> console.log(error))
+    }
+
+    function crearUsuario() {
+        fetch(urlAPI4geeks+'/users/agusotoholt', {
+            method:'POST',
+            headers:{"Content-Type": "application/json"}
+        })
+        .then((response)=> response.json())
+        .then((data)=> console.log(data))
+        .catch((error)=> console.log(error))
+    }
+
+//aca antes del return van los useEffect
+
+    useEffect (() => {
+        // crearUsuario(); //lo comento porque el usuario ya esta creado, pero hay que hacerlo al ejecutarla un nuevo dia
+        obtenerTareas();
+    }, [])
 
 	return (
 		<div className="text-center mt-4">
@@ -28,13 +76,13 @@ const Home = () => {
                     <p>No tasks, add a task</p>
                 ) : (
                     <ul className="list-group">
-                        {list.map((valor, index) => (
+                        {list.map((item, index) => (
                             <li 
                             key={index} 
                             className="list-group-item d-flex justify-content-between align-items-center" 
                             onMouseEnter={() => setHoverIndex(index)} 
                             onMouseLeave={() => setHoverIndex(null)}>
-                            {valor}{hoverIndex === index ? (<button type="button" className="btn-close float-end" aria-label="Close" onClick={() => deleteTarea(index)}></button>) : null}
+                            {item.label}{hoverIndex === index ? (<button type="button" className="btn-close float-end" aria-label="Close" onClick={() => {borrarTareas(item); obtenerTareas();}}></button>) : null}
                             </li>
                         ))}
                         <li className="list-group-item text-muted small text-start">{list.length} {list.length === 1 ? 'item left' : 'items left'}</li>
